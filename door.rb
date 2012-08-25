964e68eedc976b85d16ffb7c41c2257eb45b65fd
David Sewell <dsewell01@live.com>
2012-08-24 23:14:06 -0500

    fixing errors

--- door.rb
+++ door.rb
@@ -27,25 +27,33 @@ class Door
         
         def paint(blue)
                 if self.is_open
-                    self.color = blue
-                    self.wet = true
+                   self.color = blue
+                   self.wet = true
                 else
                         p "open the door before painting"
                 end
-        end
-        
+        end         
+                    
         def close_door
                 if (self.wet == true)
                         p "please don't close wet doors"
                 else
-                  #open/close door code blocks here
+                end
+        end
+
+        def open_door
+                   self.open = true
+                        p "The door is open"
+        end        
+                    
+        def close_door
+                   self.open = false
+                        p "The door is closed"
                   #front_door.paint ("blue") - method
                   #front_door.dry - method
                   #front_door.paint( 3, :red ).dry( 30 ).close(
-                end
         end
 end
-
 front_door = Door.new
 p "closing the door"
 front_door.close_door
